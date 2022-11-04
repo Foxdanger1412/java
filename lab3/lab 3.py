cd7ed32fc29c47dd0a7b215f12a733caa136985a
AhmedEldeeb <eldeeb757575@gmail.com>
2022-11-04 16:46:52 +0200

    added lab3

---
+++ lab3/lab 3.py
@@ -0,0 +1,34 @@
+
+## code for getting input from the usen in the right format
+size = input()
+list = size.split(', ')
+n = int(list[0])
+m = int(list[1])
+matrix = []
+for i in range(n):
+    inputt = input()
+    arr = [str(x) for x in inputt]
+    matrix.append(arr)
+id = input()
+thre = input()
+
+## locations of id in the 2D matrix
+loc_id = []
+for i in range(n):
+    indices = []
+    for idx, value in enumerate(matrix[i]):
+        if value == str(id):
+            indices.append([i,idx])
+    print(indices)
+    ##loc_id.append(indices)
+
+
+
+
+
+
+
+      
+
+
+    
